c7436e9ff184104eef97853b19b133f21ed6fe66
mike <m.y.wang@umail.leidenuniv.nl>
2023-04-03 16:35:05 +0200

    got bin sizes

--- test/getrate.py
+++ test/getrate.py
@@ -9,5 +9,33 @@ Created on Mon Apr  3 13:50:35 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import ROOT 
+max_no_xbins = 31
 
 hit_data = ROOT.TFile.Open("jra_133_14307.root")
+data2 = hit_data.Detector.DU10.F10.h_pmt_rate_distributions_Summaryslice # 31 x 100 bins
+
+print(data2.GetNbinsX())
+
+hit_rate_test = np.zeros([max_no_xbins, 100])
+
+
+yaxis = data2.GetYaxis()
+#Get approximation of binwidths
+binarray = np.zeros([2, 100])
+for i in range(0, 100):
+    binarray[0, i] = yaxis.GetBinWidth(i)
+print(yaxis.GetBinWidth(20))
+
+binarray[1, :] = np.cumsum(binarray[0,:])
+print(binarray)
+
+for i in range(0, max_no_xbins):
+    for j in range(0, 100):
+        hit_rate_test[i, j] = data2.Integral(i, i+1, j, j+1)
+        
+
+plt.imshow(hit_rate_test)
+plt.show()
+    
+
+
