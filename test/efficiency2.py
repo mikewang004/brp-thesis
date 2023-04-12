7580db9883dae78f05c62934a49a6f0f7628492f
mikewang004 <mikewang1@icloud.com>
2023-04-12 14:39:44 +0200

    test

--- test/efficiency2.py
+++ test/efficiency2.py
@@ -17,8 +17,8 @@ def gauss(x, a, x0, sigma):
 def exp_func(x, a, b, c):
     return a*np.exp(b*x) + c
 
-def lin_func(x, a):
-    return a*x 
+def lin_func(x, a, b):
+    return a*x + b
 
 class gaussfit():
     """Routine to get gaussian data fitted and plotted. Only requires
@@ -56,7 +56,61 @@ class gaussfit():
         mean = self.mean()
         return mean, gauss(mean, *popt)
     
+class meanhitrate():
+    """Everything that has to do with the efficiency/rate arrays"""
+    def __init__(self, mean_hit_rate):
+        """Remember mean hit rate array has following structure:
+            eff / pmt / du / floor / mean or something"""
+        self.meanhitrate = mean_hit_rate
+        
+    def avg_top_bottom_pmts(self, mid_pmt):
+        self.meanhitrate = self.meanhitrate[~np.isnan(self.meanhitrate).any(axis=1)]
+        self.meanhitrate = self.meanhitrate.reshape(int(self.meanhitrate.shape[0]/31), 31, 5)
+        top_avg = np.mean(self.meanhitrate[:, 0:mid_pmt, :], axis=1)
+        bottom_avg = np.mean(self.meanhitrate[:, mid_pmt:31, :], axis=1)
+        self.top_avg = top_avg; self.bottom_avg = bottom_avg
+        return self.top_avg, self.bottom_avg
+    
+    def plot_top_bottom_pmts(self):
+        top_mask = self.top_avg[:-1] == self.top_avg[1:]
+        j = 0
+        xfit = np.linscape(0, 1.4, 100)
+        for i in range(0, self.top_avg.shape[0]-1):
+            if top_mask[i,2] == False: #checks if new du starts. 
+                #Concatenate arrays together
+                xy = np.zeros([2, np.abs(j-i)*2])
+                halfway = np.abs(j-i)
+                xy[:, :halfway] = self.top_avg[j:i, 0], self.top_avg[j:i, 4]
+                xy[:, halfway:halfway*2] = self.bottom_avg[j:i, 0], self.bottom_avg[j:i, 4]
+                popt,pcov = sp.curve_fit(lin_func, xy[0, :halfway], xy[1, :halfway])
+                popt2, pcov2 = sp.curve_fit(lin_func, xy[0, halfway:], xy[1, halfway:])
+                plt.plot(xfit, lin_func(xfit, *popt), label="top fit")
+                plt.plot(xfit, lin_func(xfit, *popt2),label="bottom fit")
+                plt.scatter(self.top_avg[j:i, 0], self.top_avg[j:i, 4], label="average of pmts 0-11")
+                plt.scatter(self.bottom_avg[j:i, 0], self.bottom_avg[j:i, 4], label="average of pmts 12-30")
+                plt.title("Rate vs efficiency for du no %i" %(self.top_avg[i, 2]))
+                plt.ylim(0, 12.5)
+                plt.xlim(0, 1.4)
+                plt.xlabel("Efficiency")
+                plt.ylabel("Rate [kHz]")
+                plt.legend()
+                plt.show()
+                j = i  
+        
+    def recombine_duf_top_bottom_pmts(self, column_no = 2):
+        """Note column_no in [0,4] corresponding to eff / pmt / du / floor / mean."""
+        max_doms_str = 18
+        top_mask = self.top_avg[:-1] == self.top_avg[1:]
+        no_dus = top_mask.shape[0] - top_mask[:, column_no].sum()
+        du_block_array = np.zeros([ no_dus, no_dus, 5])
+        for i in range(0, self.top_avg.shape[0]-1):
+            if top_mask[i,column_no] == False: #checks for start new du.
+                pass
+        
+    
 def fit_bin_size(filename):
+    """Corrects for the fact that the y-bin sizes in the ROOT data are exponential.
+    Returns fit parameters for a function y = a*exp(b*x) + c"""
     binsizes = np.loadtxt(filename)
     xbins = np.arange(0, len(binsizes[:, 0]))
     #fit now binsize to some sort of relation 
@@ -126,48 +180,17 @@ def calc_hit_rate(mapdata):
             dufloorhitrate = get_du_floor_data(dufloordata, 31)
             for k in range(0, 31):
                 test = gaussfit(ytest, dufloorhitrate[:, k])
-                mean_hit_rate[j + k, 4] = exp_func(test.get_mean_coords()[0], *bin_popt)
+                mean_hit_rate[j + k, 4] = exp_func(test.get_mean_coords()[0], *bin_popt) 
+                #to convert bins to real unit as the y-bin size is log
         else:
                 mean_hit_rate[j:j+31, 4] = np.nan
         j = j + 31
     return mean_hit_rate
 
 
-def avg_top_bottom_pmts(mean_hit_rate, mid_pmt):
-    mean_hit_rate = mean_hit_rate[~np.isnan(mean_hit_rate).any(axis=1)]
-    mean_hit_rate = mean_hit_rate.reshape(int(mean_hit_rate.shape[0]/31), 31, 5)
-    top_avg = np.mean(mean_hit_rate[:, 0:mid_pmt, :], axis=1)
-    bottom_avg = np.mean(mean_hit_rate[:, mid_pmt:31, :], axis=1)
-    return top_avg, bottom_avg
 
-def recombine_duf_top_bottom_pmts(top_avg, bottom_avg):
-    n_groups = 2
-    top_mask = top_avg[:-1] == top_avg[1:]
-    no_doms = top_mask.shape[0]-np.count_nonzero(top_mask[:, 2])
-    xfit = np.linspace(0, 1.4, 100)
-    #grouped_doms = np.zeros([no_doms, 18, 2 + n_groups*3])
-    j = 0
-    for i in range(0, top_avg.shape[0]-1):
-        if top_mask[i,2] == False:
-            #Concatenate arrays together
-            xy = np.zeros([2, np.abs(j-i)*2])
-            halfway = np.abs(j-i)
-            xy[:, :halfway] = top_avg[j:i, 0], top_avg[j:i, 4]
-            xy[:, halfway:halfway*2] = bottom_avg[j:i, 0], bottom_avg[j:i, 4]
-            popt,pcov = sp.curve_fit(lin_func, xy[0, :halfway], xy[1, :halfway])
-            popt2, pcov2 = sp.curve_fit(lin_func, xy[0, halfway:], xy[1, halfway:])
-            plt.plot(xfit, lin_func(xfit, *popt), label="top fit")
-            plt.plot(xfit, lin_func(xfit, *popt2),label="bottom fit")
-            plt.scatter(top_avg[j:i, 0], top_avg[j:i, 4], label="average of pmts 0-11")
-            plt.scatter(bottom_avg[j:i, 0], bottom_avg[j:i, 4], label="average of pmts 12-30")
-            plt.title("Rate vs efficiency for du no %i" %(top_avg[i, 2]))
-            plt.ylim(0, 12.5)
-            plt.xlim(0, 1.4)
-            plt.xlabel("Efficiency")
-            plt.ylabel("Rate [kHz]")
-            plt.legend()
-            plt.show()
-            j = i
+
+
     
 def main():
     str_effs = "data-133-144-eff.txt"; str_effs_map = "map.txt"
@@ -176,8 +199,9 @@ def main():
     eff_map, mapdata = read_mapdata(str_effs, str_effs_map, low_pmt, high_pmt)
     mapdata = mapdata[np.lexsort((mapdata[:, 3], mapdata[:, 2]))]
     mean_hit_rate = (calc_hit_rate(mapdata))
-    top_avg, bottom_avg = avg_top_bottom_pmts(mean_hit_rate, mid_pmt)
-    recombine_duf_top_bottom_pmts(top_avg, bottom_avg)
+    test = meanhitrate(mean_hit_rate)
+    top_avg, bottom_avg = test.avg_top_bottom_pmts(mid_pmt)
+    test.recombine_duf_top_bottom_pmts()
     
 if __name__ == "__main__":
     main()
