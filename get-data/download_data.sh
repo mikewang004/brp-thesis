d899fd351557f4e330b00e4fd375a1652d95ef4d
Mike Wang <mwang@cca007.in2p3.fr>
2023-04-19 13:44:28 +0200

    script synchronisation

--- get-data/download_data.sh
+++ get-data/download_data.sh
@@ -4,5 +4,5 @@ declare -i enddata=14442
 z=$(( enddata - startdata ))
 for (( i = 0; i <= $z; i++ ))
 do
-    echo $((i + $startdata))
+    JRunAnalyzer -f root://ccxroot:1999//hpss/in2p3.fr/group/km3net/data/raw/sea/KM3NeT_00000133/14/KM3NeT_00000133_000$z.root -a /pbs/throng/km3net/detectors/KM3NeT_00000133_20221025.detx -o jra_133_$z.root -@ "p0=1"
 done
