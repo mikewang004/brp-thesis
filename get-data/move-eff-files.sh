d6c8f381283e847367a78ee799552750fbe7a83e
Mike Wang <mikewang004@gmail.com>
2023-04-19 16:02:59 +0200

    script sync

---
+++ get-data/move-eff-files.sh
@@ -0,0 +1,10 @@
+module load jpp/master
+declare -i startdata=14399
+declare -i enddata=14422
+z=$(( enddata - startdata ))
+cd /sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/
+for (( i = $startdata; i <= $enddata; i++ ))
+do
+    cp KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt /pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
+done
+
