d7457ae4079c7cca06cb1c483ce452bfd9a4678c
Mike Wang <mwang@cca007.in2p3.fr>
2023-04-19 17:08:44 +0200

    sync

--- get-data/move-eff-files.sh
+++ get-data/move-eff-files.sh
@@ -1,9 +1,13 @@
+#!/bin/sh
+
 module load jpp/master
 declare -i startdata=14399
-declare -i enddata=14422
-z=$(( enddata - startdata ))
+declare -i enddata=14400
 for (( i = $startdata; i <= $enddata; i++ ))
 do
-    cp ../../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt ~/pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
+    echo "$i"
+    cd /
+    cd /sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/
+    cp KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt  ../../../../../../../pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
 done
 
---
+++ get-data/test.sh
@@ -0,0 +1,3 @@
+cd /
+cd /sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/
+ls
