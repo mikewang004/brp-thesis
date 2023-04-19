e978d31902df09807f86cab5ace071ab29adf528
Mike Wang <mikewang004@gmail.com>
2023-04-19 16:24:53 +0200

    test

--- get-data/move-eff-files.sh
+++ get-data/move-eff-files.sh
@@ -4,6 +4,6 @@ declare -i enddata=14422
 z=$(( enddata - startdata ))
 for (( i = $startdata; i <= $enddata; i++ ))
 do
-    cp ~/sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt ~/pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
+    cp ../../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt ~/pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
 done
 
