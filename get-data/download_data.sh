be60eaf0976a2cb8a43ba62d6c1ab9b40a766fa7
Mike Wang <mwang@cca007.in2p3.fr>
2023-04-19 15:20:31 +0200

    commited first job

--- get-data/download_data.sh
+++ get-data/download_data.sh
@@ -1,7 +1,9 @@
 module load jpp/master
 declare -i startdata=14399
-declare -i enddata=14421
+declare -i enddata=14422
+z=$(( enddata - startdata ))
+for (( i = $startdata; i <= $enddata; i++ ))
 do
-    JRunAnalyzer -f root://ccxroot:1999//hpss/in2p3.fr/group/km3net/data/raw/sea/KM3NeT_00000133/14/KM3NeT_00000133_000$z.root -a /pbs/throng/km3net/detectors/KM3NeT_00000133_20221025.detx -o jra_133_$z.root -@ "p0=1"
+    JRunAnalyzer -f root://ccxroot:1999//hpss/in2p3.fr/group/km3net/data/raw/sea/KM3NeT_00000133/14/KM3NeT_00000133_000$i.root -a /pbs/throng/km3net/detectors/KM3NeT_00000133_20221025.detx -o jra_133_$i.root -@ "p0=1"
 done
 
--- get-data/lyon_superscript.sh
+++ get-data/lyon_superscript.sh
@@ -1,7 +1,7 @@
-#!bin/sh
+#!/bin/sh
 #
 #SBATCH --job-name="JRunAnalysis"
-#SBATCH --partition=compute
+#SBATCH --partition=htc
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
