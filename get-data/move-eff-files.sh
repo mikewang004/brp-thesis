4b1b0b9e0fa8dfb59a86b137d1b57aa75b0a541f
Mike Wang <mikewang004@gmail.com>
2023-04-19 17:12:41 +0200

    new eff script

--- get-data/move-eff-files.sh
+++ get-data/move-eff-files.sh
@@ -1,9 +1,29 @@
-module load jpp/master
-declare -i startdata=14399
-declare -i enddata=14422
-z=$(( enddata - startdata ))
-for (( i = $startdata; i <= $enddata; i++ ))
-do
-    cp ../../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt ~/pbs/home/m/mwang/zee-symfonie/get-data/KM3NeT_00000133_000%i.v8.0_PMTeff_new.ToT.QE.PMTeff.txt
+#!/bin/bash
+
+# Usage: ./move_files.sh <start_number> <end_number> <source_folder>
+
+start=14399
+end=14422
+source_folder=../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/
+destination_folder=$(dirname "$0")/
+
+if [ ! -d "$destination_folder" ]; then
+  echo "Error: Destination folder does not exist."
+  exit 1
+fi
+
+if [ ! -d "$source_folder" ]; then
+  echo "Error: Source folder does not exist."
+  exit 1
+fi
+
+for file in "$source_folder"/*ToT.QE.PMTeff.txt; do
+  filename=$(basename "$file")
+  number=$(echo "$filename" | grep -oE '[0-9]+')
+  if [ -n "$number" ] && (( number >= start && number <= end )); then
+    mv "$file" "$destination_folder"
+    echo "Moved $filename to $destination_folder"
+  fi
 done
 
+echo "File movement completed."
