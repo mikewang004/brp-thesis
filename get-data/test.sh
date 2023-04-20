76661bee4b552168f1eeee748f32bae8e4da7513
Mike Wang <mwang@cca009.in2p3.fr>
2023-04-20 13:41:05 +0200

    got eff pull script working

--- get-data/test.sh
+++ get-data/test.sh
@@ -1,3 +1,8 @@
-cd /
-cd /sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/
-ls
+declare -i startdata=14399
+declare -i enddata=14422
+for (( i = $startdata; i <= $enddata; i++ ))
+do
+    declare  filename="KM3NeT_00000133_000${i}.v8.0_PMTeff_new.ToT.QE.PMTeff.txt"
+    cp /../../../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/$filename ./$filename
+done
+
