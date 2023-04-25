231b9888edc00d71cafe8a5b250b01175e367ebc
Mike Wang <mwang@cca002.in2p3.fr>
2023-04-25 12:32:53 +0200

    runs 133-14423 up to 14456

--- get-data/download_data.sh
+++ get-data/download_data.sh
@@ -1,6 +1,6 @@
 module load jpp/master
-declare -i startdata=13504
-declare -i enddata=13560
+declare -i startdata=14423
+declare -i enddata=14459
 for (( i = $startdata; i <= $enddata; i++ ))
 do
     JRunAnalyzer -f root://ccxroot:1999//hpss/in2p3.fr/group/km3net/data/raw/sea/KM3NeT_00000133/14/KM3NeT_00000133_000$i.root -a /pbs/throng/km3net/detectors/KM3NeT_00000133_20221025.detx -o jra_133_$i.root -@ "p0=1"
--- get-data/lyon_superscript.sh
+++ get-data/lyon_superscript.sh
@@ -5,6 +5,6 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=2G
 
 ./download_data.sh
