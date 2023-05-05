74781ae278440a15034f506a07210b2c2533dffe
mike <m.y.wang@umail.leidenuniv.nl>
2023-05-05 12:43:13 +0200

    test

--- strand-alleen/test.sh
+++ strand-alleen/test.sh
@@ -3,6 +3,6 @@ declare -i enddata=3103
 for (( i = $startdata; i <= $enddata; i++ ))
 do
     declare  filename="mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.${i}.root"
-    cp /../../../../../../../sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/$filename ./showerdata/$filename
+    cp /../../../../../../../sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/$filename /showerdata/$filename
 done
 
