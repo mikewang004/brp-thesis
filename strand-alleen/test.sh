2e365457498b9f425be790145ccb8ae0f1d28215
mike <m.y.wang@umail.leidenuniv.nl>
2023-05-05 12:41:33 +0200

    test

--- strand-alleen/test.sh
+++ strand-alleen/test.sh
@@ -1,8 +1,8 @@
-\declare -i startdata=14423
-declare -i enddata=14457
+\declare -i startdata=3092
+declare -i enddata=3103
 for (( i = $startdata; i <= $enddata; i++ ))
 do
-    declare  filename="KM3NeT_00000133_000${i}.v8.0_PMTeff_new.ToT.QE.PMTeff.txt"
-    cp /../../../../../../../sps/km3net/repo/data/calibration/KM3NeT_00000133/v8.0_PMTeff_new/calibration/$filename ./$filename
+    declare  filename="mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.${i}.root"
+    cp /../../../../../../../sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/$filename ./showerdata/$filename
 done
 
