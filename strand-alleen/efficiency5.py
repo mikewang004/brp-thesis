65af4e89c345d870fef72670110a9aae687a79d0
Mike Wang <mikewang004@gmail.com>
2023-05-30 15:33:44 +0200

    prepared seperation real/simulated data

--- strand-alleen/efficiency5.py
+++ strand-alleen/efficiency5.py
@@ -23,6 +23,7 @@ class map_hit_data():
     def __init__(self, muon_hit_data, pmt_id_map):
         self.muon_hit_data = muon_hit_data
         self.modid_map = modid_map
+        self.mod_id_to_floor_string()
 
     def mod_id_to_floor_string(self):
         """Note output is of the form: amount of mod-ids / pmts numbers / [str no; floor no; mod-id; pmt-no; no. hits]"""
@@ -83,48 +84,6 @@ class map_hit_data():
                 str_floor_length[l] = i - k
                 k = i; l = l + 1
         return pmt_group_pairs, str_floor_length
-
-    def heatmap_array(self, indices):
-        pmt_group = 0
-        pmt_group_mean = self.normalise_over_n_pmts(indices)
-        pmt_group_mean_sorted = pmt_group_mean
-        #pmt_group_pairs = pmt_group_mean[:, 0, :]
-        for m in range(0, len(indices)-1):
-            pmt_group_pairs = pmt_group_mean[:, m, :]
-            pmt_group_mean_sorted[:, m, :], str_floor_length = self.heatmap_averages_single_loop(pmt_group_pairs)
-        floorlist = np.unique(pmt_group_mean_sorted[:, pmt_group, 1]); stringlist = np.unique(pmt_group_mean_sorted[:, pmt_group, 0])
-        heatmap = np.zeros([len(floorlist), len(np.unique(stringlist))])
-        print(np.unique(stringlist))
-        #TODO fill in heatmap and account for nans.
-        k = 0; m = 0; x = 0; j = 0; l = 0
-        for i in range(1, pmt_group_mean_sorted.shape[0]): #first fill in the x/string direction
-        #for i in range(1, 70):
-            #New approach: floorlist index is not the same as pmt group mean sorted index. 
-            if pmt_group_mean_sorted[i, pmt_group, 0] != pmt_group_mean_sorted[i-1, pmt_group, 0] or i == pmt_group_mean_sorted.shape[0]-1:
-                while j < len(floorlist): #checks if all floor/string combinations are extant
-                    #print(pmt_group_mean_sorted[k + l, 0, 1])
-                    #print(j, l)
-                    if floorlist[j] == pmt_group_mean_sorted[k + l, pmt_group, 1]: #case if combination is extant 
-                        heatmap[j, m] = pmt_group_mean_sorted[k + l, pmt_group, 4]
-                        l = l + 1
-                    else: #case if combination non-extant 
-                        heatmap[j, m] = np.nan
-                        x = x + 1
-                    j = j + 1
-                #print(pmt_group_mean_sorted[k:i, 0, :2])
-                m = m + 1; k = i; j = 0; l = 0
-        return heatmap, floorlist, stringlist
-    
-    def heatmap_averages(self, indices):
-        heatmap, floorlist, stringlist = self.heatmap_array(indices)
-        #Flip heatmap and floorlist in y-direction as floor 1 is closest to seabed.
-        #heatmap = np.flipud(heatmap); floorlist = np.flipud(floorlist)
-        fig = px.imshow(heatmap, labels=dict(x="string number",y="floor number", color="number of hits"), 
-                        x= stringlist,y = floorlist,
-                        title = "Rates of PMTs per DOM, PMTs 0-11", 
-                        text_auto=True, aspect="auto", width=2560, height=1440, origin="lower")
-        fig.write_image("mod-id-pmt-plot-upper-test.pdf")
-        return 0;
 
     def heatmap_array_single_group(self, pmt_group_mean_sorted, pmt_group_no, heatmap, floorlist, stringlist):
         k = 0; m = 0; x = 0; j = 0; l = 0
@@ -190,67 +149,12 @@ class map_hit_data():
         for i in range(0, len(indices)-1):
         #for i in range(0, 1):
             self.heatmap_equal_bins_single_loop(heatmap[i, :, :], indices, i, stringlist, floorlist)
-        
-    def heatmap_averages_backup(self, indices):
-        pmt_group_mean = self.normalise_over_n_pmts(indices)
-        pmt_group_mean_sorted = pmt_group_mean
-        #pmt_group_pairs = pmt_group_mean[:, 0, :]
-        for m in range(0, len(indices)-1):
-            pmt_group_pairs = pmt_group_mean[:, m, :]
-            pmt_group_mean_sorted[:, m, :], str_floor_length = self.heatmap_averages_single_loop(pmt_group_pairs)
-        np.savetxt("pmt_string_sorted.txt", np.array(pmt_group_mean_sorted[:, 0, :2], dtype=int))
-        floorlist = np.unique(pmt_group_mean_sorted[:, 0, 1]); stringlist = np.unique(pmt_group_mean_sorted[:, 0, 0])
-        heatmap = np.zeros([len(floorlist), len(np.unique(stringlist))])
-        #TODO fill in heatmap and account for nans.
-        k = 0; m = 0; x = 0; j = 0;
-        #for i in range(1, pmt_group_mean_sorted.shape[0]): #first fill in the x/string direction
-        for i in range(1, 70):
-            #New approach: floorlist index is not the same as pmt group mean sorted index. 
-            if pmt_group_mean_sorted[i, 0, 0] != pmt_group_mean_sorted[i-1, 0, 0]:
-                while j < len(floorlist):
-                    print("current value of j is " + str(j))
-                    #TODO if first floor does not exist a lag in which floors do not exist will occur. 
-                    if floorlist[j] == pmt_group_mean_sorted[k+j, 0, 1]:
-                        heatmap[j, m] = pmt_group_mean_sorted[k+j, 0, 4]
-                        print("correct values"+ str((stringlist[m], floorlist[j], pmt_group_mean_sorted[k+j, 0, :2])))
-                        if pmt_group_mean_sorted[k+j, 0, 1] == np.amax(pmt_group_mean_sorted[k:i, 0, 1]): #breaks loop at max floor no
-                            break
-                        j = j + 1
-                        
-                    else:
-                        x = x + 1
-                        print(j)
-                        print(stringlist[m], floorlist[j], pmt_group_mean_sorted[k+j, 0, :2])
-                        heatmap[j, m] = np.nan
-                        print("diff is " + str(np.abs(floorlist[j] - pmt_group_mean_sorted[k + j, 0, 1])))
-                        
-                        j = j + int(np.abs(floorlist[j] - pmt_group_mean_sorted[k + j, 0, 1]-1))
-                    print("new value j=" + str(j))
-                    print(" ")
-                    #print(heatmap[j, m])
-                print(pmt_group_mean_sorted[k:i, 0, :2])
-                m = m + 1; k = i; j = 0
-
-
-
-
-
-
-def check_for_unique_pairs(a):
-        """Takes an [n, 2] sized array and compares if any combination of the pairs across the 2-dimension 
-        occurs again."""
-        #ctr = Counter(frozenset(x) for x in a)
-        #b = [ctr[frozenset(x)] == 1 for x in a]
-        #print(b)
-        b = np.unique(a, axis = 0)
-        print(b.shape)
 
 
 
 
 indices = [0, 11, 18, 30]
 test = map_hit_data(muon_hit_data, modid_map)
-test.mod_id_to_floor_string()
 #test.heatmap_equal_bins(indices)
 test.pmt_group_data_to_heatmap(indices)
 #test.heatmap_averages(indices)
