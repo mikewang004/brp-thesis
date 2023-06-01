329b26915d318bfc8cf3d01e1af59161ebf6a1f1
Mike Wang <mwang@cca011.in2p3.fr>
2023-06-01 16:46:48 +0200

    automatisation t0 sim events finder

--- strand-alleen/wrapper_muonsim.py
+++ strand-alleen/wrapper_muonsim.py
@@ -10,7 +10,7 @@ import numpy as np
 aashower_startno = 137
 infile_real = []
 for i in range(0, 10):
-    infile_real.append("/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/data/KM3NeT_00000133/v8.1.1/reco/datav8.1.1.jchain.aashower.00%i%i.root" %(aashower_startno, 50 + i))
+    infile_real.append("/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/data/KM3NeT_00000133/v8.1.1/reco/datav8.1.1.jchain.aashower.000%i%i.root" %(aashower_startno, 50 + i))
 
 
 infile = ["/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.3092.root", 
