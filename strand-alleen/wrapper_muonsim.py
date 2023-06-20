5702ecdb593557927537f0e72a46134d24106720
Mike Wang <mwang@cca001.in2p3.fr>
2023-06-20 17:14:02 +0200

    ratio data new binning

--- strand-alleen/wrapper_muonsim.py
+++ strand-alleen/wrapper_muonsim.py
@@ -29,7 +29,7 @@ infile = ["/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm
 #infile = ["/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.3092.root", "/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.3093.root"]
 #used numbers for above line are 3092-3 
 #infile = "/sps/km3net/repo/data_processing/tag/v8.1/data_processing/prod/mc/atm_muon/KM3NeT_00000133/v8.1/reco/mcv8.1.mupage_tuned_100G.sirene.jterbr00013754.jchain.aashower.3092.root"
-f = EventFile( infile )
+f = EventFile( infile_real )
 
 
 rmap = residual_map()
@@ -59,8 +59,8 @@ hit_array = np.zeros([len(list(rmap.domids())),no_pmts,  3])
 #dom-ids / pmt no / no-hits 
 domid_map = list(rmap.domids())
 print("test if the map is working properly")
-lowerbound, upperbound = -50, 60
-#lowerbound, upperbound = -50, 150
+#lowerbound, upperbound = -50, 60
+lowerbound, upperbound = -10, 20
 
 for j in range(0, len(domid_map)):
     """Structure as follows: [dom number, pmt-number, amount of hits]"""
@@ -71,12 +71,12 @@ for j in range(0, len(domid_map)):
 #Try DOM 1, 8, 18; 809526109, 808985061, 808984586
 #corresponds to line no. 254, 197, 189 in the array.  
 
-list = [253, 196, 188]
-for i in range(2, 3):
-    f = rmap.geth(domid_map[list[i]], 0)
-    for j in range(0, 18):
-        f = f + rmap.geth(domid_map[list[i]],j) 
-    f.Draw()
-#np.save("muon_hit_data_real-001375x.npy", hit_array)
-#np.save("muon_hit_data-sim-13754.npy", hit_array)
-np.savetxt("domid-map.txt", domid_map)
+#list = [253, 196, 188]
+#for i in range(2, 3):
+#    f = rmap.geth(domid_map[list[i]], 0)
+#    for j in range(0, 18):
+#        f = f + rmap.geth(domid_map[list[i]],j) 
+#    f.Draw()
+#np.save("muon_hit_data_real-reduced_bins-001375x.npy", hit_array)
+np.save("muon_hit_data-sim-reduced_bins-13754.npy", hit_array)
+#np.savetxt("domid-map.txt", domid_map)
