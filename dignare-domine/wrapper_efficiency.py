f005050f81678b5a44e2a6befab2d51d2394e59e
mike <m.y.wang@umail.leidenuniv.nl>
2023-07-07 00:54:13 +0200

    new plots

--- dignare-domine/wrapper_efficiency.py
+++ dignare-domine/wrapper_efficiency.py
@@ -42,8 +42,10 @@ real_new_heatmap = heatmap(real_map_new_pmts, floorlist, stringlist)
 
 #sim_ratio_map.compare_upper_lower_pmts_heatmap(indices, "Ratio of upper/lower PMTs ratio of simulated/real rates")
 #sim_ratio_map.plot_heatmap(indices, "Ratio of simulated vs real rates of PMTs per DOM")
-real_heatmap.plot_heatmap(indices, pmt_letters, "Sum of all hits per DOM for indicated PMT group, real data", save_map = "plots")
-real_new_heatmap.plot_heatmap(indices, pmt_letters, "Sum of all hits per DOM for indicated PMT group, real data, new PMTs", save_map = "plots")
+
+#real_heatmap.plot_heatmap(indices, pmt_letters, "Sum of all hits per DOM for indicated PMT group, real data", save_map = "plots")
+#real_new_heatmap.plot_heatmap(indices, pmt_letters, "Sum of all hits per DOM for indicated PMT group, real data, new PMTs", save_map = "plots")
+
 #Create new dataset by laying the efficiency map over the ratio map 
 
 
@@ -52,3 +54,28 @@ print(real_new_heatmap.get_avg_std(indices))
 sim_ratio_eff_map = np.zeros([2,sim_eff_map.shape[0], sim_eff_map.shape[1], sim_eff_map.shape[2]])
 sim_ratio_eff_map[0, :, :, :] = sim_ratio_map.heatmap[:, :, :]
 sim_ratio_eff_map[1, :, :, :] = eff_heatmap.heatmap[:, :, :]
+
+letters_ints = np.arange(0, len(pmt_letters))
+
+
+old_pmt_mean, old_pmt_std = real_heatmap.get_avg_std(indices)
+new_pmt_mean, new_pmt_std = real_new_heatmap.get_avg_std(indices)
+#plt.errorbar(letters_ints, old_pmt_mean, yerr = old_pmt_std, fmt="o")
+#plt.errorbar(letters_ints, new_pmt_mean, yerr = new_pmt_std, fmt=".")
+#plt.xticks(np.arange(0, len(letters_ints)), pmt_letters)
+#plt.show()
+
+#Now create subplots 
+
+fig, axs = plt.subplots(2, 1, sharey=True)
+plt.setp(axs, xticks = letters_ints, xticklabels = pmt_letters, ylabel= "mean hit rate")
+axs[0].errorbar(letters_ints, old_pmt_mean, yerr = old_pmt_std, fmt="o")
+axs[1].errorbar(letters_ints, new_pmt_mean, yerr = new_pmt_std, fmt="o")
+
+fig.suptitle("Mean of the sum of the DOM hit rate seperated by ring") 
+axs[0].set_title("Old PMTs (model R12199)")
+axs[1].set_title("New PMTs (model R14374)")
+plt.subplots_adjust(hspace=0.4)
+plt.savefig("plots/mean-sum-doms-pmt-models.pdf")
+plt.show()
+
